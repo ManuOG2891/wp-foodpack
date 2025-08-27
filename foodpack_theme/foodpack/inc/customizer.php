e3e9823df542946a3a4857afcefbeaadc9dfd8dc
ManuOG2891 <mhrifcas@gmail.com>
2025-08-27 13:19:26 -0400

    Carga de nuevo tema _s con compatibilidad Woocommerce y _sassify!

---
+++ foodpack_theme/foodpack/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * FoodPack Theme Theme Customizer
+ *
+ * @package FoodPack_Theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function foodpack_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'foodpack_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'foodpack_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'foodpack_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function foodpack_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function foodpack_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function foodpack_customize_preview_js() {
+	wp_enqueue_script( 'foodpack-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'foodpack_customize_preview_js' );
